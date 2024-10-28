edcc2db795d26389304f1c3cf19966c18f80ac38
Jesse Herring <123990927+jesjess243@users.noreply.github.com>
2024-10-28 17:47:45 -0400

    Add files via upload

---
+++ indexRouter.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", {messages: messages});
+});
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+    console.log("Post requested!");
+    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+    console.log("Pushed!");
+    res.redirect("/");
+});
+
+indexRouter.get("/:messageId", (req, res) => {
+    console.log(req.params);
+    res.render("index", {messages: messages.slice(req.params.messageId, req.params.messageId + 1)});
+});
+
+module.exports = indexRouter;
