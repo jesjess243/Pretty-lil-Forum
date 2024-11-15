5189ecb772cca0e22543444cd0bb3ad71d3b801d
jesjess243 <jesseherring2.19@gmail.com>
2024-11-15 17:24:09 -0500

    modified:   db/pool.js fixed data breach

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-    connectionString: "postgresql://prettylildb_owner:3thyxdB7nFCL@ep-curly-shadow-a5i9gspv.us-east-2.aws.neon.tech/prettylildb?sslmode=require"
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_NAME}.us-east-2.aws.neon.tech/prettylildb?sslmode=require`
 });
