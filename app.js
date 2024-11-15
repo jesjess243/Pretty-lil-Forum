15aa9d997360c43cd65762367b450ff805148e44
jesjess243 <jesseherring2.19@gmail.com>
2024-11-15 17:29:32 -0500

    modified:   app.js Add environment support

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// app.js: root
+const env = require("dotenv").config();
 const express = require("express");
 const path = require("node:path");
 const pg = require("pg");
@@ -5,11 +7,12 @@ const pg = require("pg");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
-app.use(express.static(__dirname));
-app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs"); // express.js
+
+app.use(express.static(__dirname)); // begin serving static files 
+app.use(express.urlencoded({ extended: true })); // begin recognizing requests as strings (encoded)
 app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
