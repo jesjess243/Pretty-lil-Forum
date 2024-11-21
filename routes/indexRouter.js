98f790acb0208cd211ed8ed6e12a2da6645badcd
jesjess243 <jesseherring2.19@gmail.com>
2024-11-20 20:40:01 -0500

    Add title comments 	modified:   css/reset.css 	modified:   css/style.css 	modified:   routes/indexRouter.js

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,3 +1,4 @@
+// routes/indexRouter.js
 const { Router } = require("express");
 const indexRouter = Router();
 
@@ -8,25 +9,5 @@ indexRouter.get("/new", indexController.msgsNewGet);
 indexRouter.get("/:messageId", indexController.msgsGet);
 
 indexRouter.post("/new", indexController.msgsNewPost);
-
-/*indexRouter.get("/", (req, res) => {
-    res.render("index", { title: "Pretty Lil Forum", messages: messages});
-});
-
-indexRouter.get("/new", (req, res) => {
-    res.render("form", {title: "New Post"});
-});
-
-indexRouter.post("/new", (req, res) => {
-    console.log("Post requested!");
-    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
-    console.log("Pushed!");
-    res.redirect("/");
-});
-
-indexRouter.get("/:messageId", (req, res) => {
-    console.log(req.params);
-    res.render("index", {title: "Pretty Lil Forum", messages: messages.slice(req.params.messageId, req.params.messageId + 1)});
-});*/
 
 module.exports = indexRouter;
